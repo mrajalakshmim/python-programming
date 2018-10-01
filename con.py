34420a89ffa9d23fb54529e2f1d44c678e4e997e
mrajalakshmim <43339891+mrajalakshmim@users.noreply.github.com>
2018-10-01 16:53:28 +0530

    Create con.py

---
+++ con.py
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class Main1
+{
+public static void main(String[] args)
+{
+char s='a';
+if(a==a||a==e||a==i||a==o||a==u)
+{
+System.out.println("Vowel");
+}
+else 
+{
+  System.out.println("Consonant");
+}
+}
