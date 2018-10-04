2d001dccbc4e605926b32fc3ea1a4f51821a9d0c
mrajalakshmim <43339891+mrajalakshmim@users.noreply.github.com>
2018-10-04 16:49:09 +0530

    Create count number of digits.py

---
+++ count number of digits.py
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int sum=0,n:
+        scanf("%d,"&n):
+            while(n!=0)
+            {
+                n=n/10;
+                sum=sum+1;
+                }
+                printf("%d",sum);
+            }
+
