0d9b78e44285028625eef33279a7480e5a787d5b
mrajalakshmim <43339891+mrajalakshmim@users.noreply.github.com>
2018-10-04 17:44:19 +0530

    Create amstrong no.py

---
+++ amstrong no.py
@@ -0,0 +1,10 @@
+n=int(input("enter a number"))
+sum=0
+temp=numberwhile temp>0:
+    digit=temp%10
+    sum+=digit**3
+    temp//=10
+    if n==sum:
+        print(n,"is an Armstrong number")
+    else:
+        print(n,"is not an Armstrong number")
