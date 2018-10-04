bd01dfbf888e425753ee5f154b6eb923835acc54
mrajalakshmim <43339891+mrajalakshmim@users.noreply.github.com>
2018-10-04 17:37:07 +0530

    Create prime no two intervels.py

---
+++ prime no two intervels.py
@@ -0,0 +1,9 @@
+Lower=int(input("Enter Lower range:"))
+upper=int(input("Enter upper range:"))
+for num in range(Lower,upper+1);
+if num>1:
+    form i in range(2,num):
+        if(num%i)==0:
+            break
+        else:
+            print(num)
