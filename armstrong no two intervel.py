64b6f7546e687bb15f87ead12810728f2763e4b9
mrajalakshmim <43339891+mrajalakshmim@users.noreply.github.com>
2018-10-04 17:50:10 +0530

    Create armstrong no two intervel.py

---
+++ armstrong no two intervel.py
@@ -0,0 +1,10 @@
+Lower=100
+upper=2000
+for num in range(Lower,upper+100):
+    order=Len(str(num))
+    tem=numwhile temp>0:
+        digit=temp%10
+        sum+=digit**order
+        temp//=10
+        if num==sum:
+            print(num)
