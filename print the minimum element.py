1d55b0833215a645f58971fdb3541582eb62250b
mrajalakshmim <43339891+mrajalakshmim@users.noreply.github.com>
2018-10-11 16:33:15 +0530

    Create print the minimum element.py

---
+++ print the minimum element.py
@@ -0,0 +1,7 @@
+def main():
+    numbers=eval(input("Give me an array of numbers:"))
+    smallest=numbers[0]
+    for i in range(0,Len(numbers),1):
+        if(numbers[i]<smallest):
+            smallest=numbers[i]
+            print("The smallest number is:",smallest)
